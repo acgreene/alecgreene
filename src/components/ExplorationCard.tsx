43ab865486716285d8825e38595e81bb42b400b4
acgreene <aleccgreene@gmail.com>
2023-03-15 23:53:47 -0400

    exploration card background color change feature.

--- src/components/ExplorationCard.tsx
+++ src/components/ExplorationCard.tsx
@@ -1,5 +1,5 @@
 import { Card, Flex, Heading, Icon } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { IconType } from "react-icons";
 
 type ExplorationCardProps = {
@@ -17,12 +17,23 @@ const ExplorationCard: React.FC<ExplorationCardProps> = ({ title, icons }) => {
   const handleMouseLeave = () => {
     setIsHovering(false);
   };
+
+  let myRef: any = useRef(null);
+  // console.log(myRef.current);
+
   return (
     <Card
+      ref={myRef}
       className="flex flex-col m-2 justify-center items-center"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      backgroundColor={isHovering ? "white" : "black"}
+      backgroundColor={
+        isHovering
+          ? `rgb(${myRef.current.offsetLeft / 5}, ${
+              myRef.current.getBoundingClientRect().top / 3
+            }, ${255})`
+          : "black"
+      }
       color={isHovering ? "black" : "white"}
       transition="0.15s"
       variant="outline"
